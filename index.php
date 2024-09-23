e60dadb952dac57c3acd0b4fe16bffe0b8911c6a
= <=>
2024-09-23 10:43:25 +0500

    php first class

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+
+// echo "hello world";
+
+// variables
+
+$name = "ebad uddin";
+
+// echo $name;
+
+$var2 = 45;
+
+$bool = true;
+
+$age = null;
+
+$float = 19.6;
+
+$arr = [1, 2, 3, 4, 5]; 
+ 
+// Data types
+/* 
+string 
+integer
+float
+double
+boolean
+null
+array
+*/
+// print_r($arr);
+// var_dump($arr);
+
+// arithmatic operators +, -, /, *, %
+
+$num1 = 46;
+$num2 = 5;
+
+$result = $num1 + $num2;
+$sub = $num1 - $num2;
+$mul = $num1 * $num2;
+$div = $num1 / $num2;
+$mod = $num1 % $num2;
+
+echo "The addition is: " . $result;
+echo "<br>";
+echo "The subtraction is: " . $sub;
+echo "<br>";
+echo "The Mulitiplication is: " . $mul;
+echo "<br>";
+echo "The division is: " . $div;
+echo "<br>";
+echo "The modulus is: " . $mod;
+
+?>
